0389fdce343fe467b81bc69f57b4651358c9df55
lukasvrenner <118417051+lukasvrenner@users.noreply.github.com>
2024-11-11 11:13:22 -0900

    print left or middle or right

--- riemann_sum.py
+++ riemann_sum.py
@@ -1,6 +1,6 @@
 def func_1(x):
     return -(x**2)/4 + 2*x
-    
+
 def riemen_sum(func, lower, upper, n: int, shift):
     delta_x = (upper - lower) / n
     sum = 0
@@ -9,6 +9,6 @@ def riemen_sum(func, lower, upper, n: int, shift):
     sum *= delta_x
     return sum
 
-print(riemen_sum(func_1, lower=3, upper=7, n=8, shift=0))
-print(riemen_sum(func_1, lower=3, upper=7, n=8, shift=0.5))
-print(riemen_sum(func_1, lower=3, upper=7, n=8, shift=1))
+print("left:",riemen_sum(func_1, lower=3, upper=7, n=8, shift=0))
+print("middle:", riemen_sum(func_1, lower=3, upper=7, n=8, shift=0.5))
+print("right:", riemen_sum(func_1, lower=3, upper=7, n=8, shift=1))
