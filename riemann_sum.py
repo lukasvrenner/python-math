64ed5822c390e00f28898b3808d58cfe7226df40
lukasvrenner <118417051+lukasvrenner@users.noreply.github.com>
2024-11-11 11:09:08 -0900

    add support for non-integer shift

--- riemann_sum.py
+++ riemann_sum.py
@@ -1,13 +1,14 @@
 def func_1(x):
     return -(x**2)/4 + 2*x
-
-def riemann_sum(func, lower, upper, n: int, shift):
+    
+def riemen_sum(func, lower, upper, n: int, shift):
     delta_x = (upper - lower) / n
     sum = 0
-    for i in range(shift, n + shift):
-        sum += func(lower + i * delta_x)
+    for i in range(n):
+        sum += func(lower + (i + shift) * delta_x)
     sum *= delta_x
     return sum
 
-print(riemann_sum(func_1, lower=3, upper=7, n=8, shift=0))
-print(riemann_sum(func_1, lower=3, upper=7, n=8, shift=1))
+print(riemen_sum(func_1, lower=3, upper=7, n=8, shift=0))
+print(riemen_sum(func_1, lower=3, upper=7, n=8, shift=0.5))
+print(riemen_sum(func_1, lower=3, upper=7, n=8, shift=1))
