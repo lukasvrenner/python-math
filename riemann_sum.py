0ce50e238bc887662f3082f2e2366cd11891e111
lukasvrenner <lukasvrenner@icloud.com>
2025-01-23 17:51:17 -0900

    feat(riemann): add support for trapezoid rule

--- riemann_sum.py
+++ riemann_sum.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-def summand(x):
-    return 5 * x ** 2
+import math
+
+def integrand(x):
+    return math.sqrt(x ** 2 + 7)
 
 def riemann_sum(func, lower, upper, n: int, shift):
     delta_x = (upper - lower) / n
@@ -11,14 +13,23 @@ def riemann_sum(func, lower, upper, n: int, shift):
     sum *= delta_x
     return sum
 
+def trapezoid(func, lower, upper, n: int):
+    delta_x = (upper - lower) / n
+
+    sum = 0
+    for i in range(1, n):
+        sum += func(lower + i * delta_x)
+    sum *= 2
+
+    sum = func(lower) + sum + func(upper)
+    return sum * delta_x / 2
+
 def simpsons(func, lower, upper, n: int):
     assert(n & 1 == 0)
     delta_x = (upper - lower) / n
 
     mul_four_sum = 0
     for i in range(1, n, 2):
-        sub = 2 * i + 1
-        print(sub)
         mul_four_sum += func(lower + i * delta_x)
     mul_four_sum *= 4
 
@@ -30,7 +41,8 @@ def simpsons(func, lower, upper, n: int):
     sum = func(lower) + mul_four_sum + mul_two_sum + func(upper)
     return sum * delta_x / 3
 
-print("left:", riemann_sum(summand, lower=8, upper=10, n=4, shift=0))
-print("middle:", riemann_sum(summand, lower=8, upper=10, n=4, shift=0.5))
-print("right:", riemann_sum(summand, lower=8, upper=10, n=4, shift=1))
-print("simpsons:", simpsons(summand, lower=8, upper=10, n=4))
+print("left:", riemann_sum(integrand, lower=0, upper=2, n=6, shift=0))
+print("middle:", riemann_sum(integrand, lower=0, upper=2, n=6, shift=0.5))
+print("right:", riemann_sum(integrand, lower=0, upper=2, n=6, shift=1))
+print("trapezoid:", trapezoid(integrand, lower=0, upper=2, n=6))
+print("simpsons:", simpsons(integrand, lower=0, upper=2, n=6))
